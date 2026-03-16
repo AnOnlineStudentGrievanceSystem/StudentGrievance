7a68fc363aef974dc37b78a5190b22de502fe1af
SHAIK FAYAZ <fayazcm047@gmail.com>
2026-03-16 11:37:47 +0530

    Admin files

--- src/main/java/com/codeverse/grievance_system/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeverse.grievance_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.codeverse.grievance_system.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-    Student findByEmail(String email);
-}
